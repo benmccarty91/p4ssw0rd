23ee0bb015e970466c8c43d33e5833a691e94b24
Ben McCarty <benmccarty91@gmail.com>
2022-12-20 10:22:03 -0700

    clean up build scripts

--- scripts/api/build.ts
+++ scripts/api/build.ts
@@ -1,11 +1,11 @@
 import { exec, ExecOptions, spawn } from 'child_process';
 
 const execOptions: ExecOptions = {
-  cwd: '../api/',
+  cwd: '../p4ssw0rd.api/',
   env: { 'DOCKER_BUILDKIT': '1' }
 }
-const projectId = process.env.PROJECT_ID ? process.env.PROJECT_ID : 'flikit-85365';
-const appName = process.env.APP_NAME ? process.env.APP_NAME : 'flikit.webapi';
+const projectId = process.env.PROJECT_ID ? process.env.PROJECT_ID : 'p4ssw0rd';
+const appName = process.env.APP_NAME ? process.env.APP_NAME : 'p4ssw0rd.api';
 const versionTag = process.env.VERSION_TAG ? process.env.VERSION_TAG : 'qa';
 const imageName = `gcr.io/${projectId}/${appName}:${versionTag}`;
 const context = '.';
